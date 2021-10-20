61704b85349a13b0bfc6b153240d742360890e42
Michael Vinicius <michaelvini62@gmail.com>
2021-10-20 18:56:00 -0300

    add button component

---
+++ front/src/Components/Button/index.js
@@ -0,0 +1,12 @@
+import { Container } from './styles';
+
+const Button = (props) => {
+  const { title, click } = props;
+  return (
+    <Container onClick={click}>
+      {title}
+    </Container>
+  );
+}
+
+export default Button;
